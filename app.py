2e71470e438018f78b8ed83d7bee9827e0d28fc0
Kaixuan Zhou <zhoukx.joseph@gmail.com>
2018-12-04 23:22:09 -0500

    authentication

--- app.py
+++ app.py
@@ -186,6 +186,8 @@ def fetchBlogs():
 @app.route("/post/blog/<item_id>")
 def detailedBlog(item_id):
     cursor = conn.cursor()
+    
+    # we need to check if the current user have access to this page or not.
 
     # content of a post
     contentItem = 'SELECT * FROM contentitem WHERE item_id = (%s)'
